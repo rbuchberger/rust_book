3646e9651c44f96d20dd84ed1938d1eaa145ef6b
Robert Buchberger <robert@buchberger.cc>
2022-05-11 17:01:45 +0200

    minigrep - Add test file & file content reading

--- 12 grep project/src/main.rs
+++ 12 grep project/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +10,8 @@ fn main() {
     println!("{:#?}", args);
     println!("Searching for {}", query);
     println!("In {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Could not read file");
+
+    println!("File contents:\n{}", contents)
 }
