792768cd924fb621f6ecd767c24ba7de089c9e0c
Robert Buchberger <robert@buchberger.cc>
2022-05-12 21:36:55 +0200

    minigrep - implement search

--- 12 grep project/src/main.rs
+++ 12 grep project/src/main.rs
@@ -11,10 +11,6 @@ fn main() {
         process::exit(1)
     });
 
-    println!("{:#?}", args);
-    println!("Searching for {}", config.query);
-    println!("In {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
