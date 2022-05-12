cffbc346495aa4c91177053179540efffa6176ac
Robert Buchberger <robert@buchberger.cc>
2022-05-12 21:22:44 +0200

    minigrep - Extract most logic to lib.rs

--- 12 grep project/src/main.rs
+++ 12 grep project/src/main.rs
@@ -1,27 +1,11 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn parse_args(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
+use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args().collect();
+
     let config = Config::parse_args(&args).unwrap_or_else(|error| {
         println!("Problem parsing arguments: {}", error);
         process::exit(1)
@@ -31,8 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Could not read file");
-
-    println!("File contents:\n{}", contents)
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
-
