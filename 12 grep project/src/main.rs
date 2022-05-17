229e831e78d5f72b2ed14670156ccbd8ec6539be
Robert Buchberger <robert@buchberger.cc>
2022-05-17 12:01:55 +0200

    minigrep - send errors to stderr

--- 12 grep project/src/main.rs
+++ 12 grep project/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args = env::args().collect();
 
     let config = Config::parse_args(&args).unwrap_or_else(|error| {
-        println!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing arguments: {}", error);
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
