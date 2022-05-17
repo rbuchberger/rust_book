cb3cc0dfb4bf852fb421c945f0deb67743f229c5
Robert Buchberger <robert@buchberger.cc>
2022-05-17 16:04:27 +0200

    minigrep - Refactor search functions
    Use iterator methods instead of for loops.

--- 12 grep project/src/lib.rs
+++ 12 grep project/src/lib.rs
@@ -20,28 +20,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines().filter(|l| l.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|l| l.to_lowercase().as_str().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
