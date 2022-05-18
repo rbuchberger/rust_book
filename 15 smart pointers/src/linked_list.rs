12a0f08ef5fc0595fe57f8c93de48e0d3f4af311
Robert Buchberger <robert@buchberger.cc>
2022-05-18 14:22:33 +0200

    Smart Pointers - multiple references via Rc

--- 15 smart pointers/src/linked_list.rs
+++ 15 smart pointers/src/linked_list.rs
@@ -1,8 +1,9 @@
-use crate::linked_list::LinkedList::{Cons, Nil};
+use crate::linked_list::LinkedList::{Nil, Pair};
+use std::rc::Rc;
 
 #[derive(Debug)]
 pub enum LinkedList {
-    Cons(i32, Box<LinkedList>),
+    Pair(i32, Rc<LinkedList>),
     Nil,
 }
 
@@ -10,13 +11,28 @@ impl LinkedList {
     pub fn new(vals: Vec<i32>) -> LinkedList {
         vals.iter()
             .rev()
-            .fold(Nil, |list, item| Cons(*item, Box::new(list)))
+            .fold(Nil, |list, item| Pair(*item, Rc::new(list)))
     }
 }
 
-pub fn demo() {
+pub fn recursion_demo() {
     // let b = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     let b = LinkedList::new(vec![1, 2, 3]);
 
     println!("{:#?}", b);
+}
+
+pub fn rc_demo() {
+    let a = Rc::new(LinkedList::new(vec![5, 10]));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
+
+    let _b = Pair(3, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let _c = Pair(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a));
+    }
+
+    println!("Count after c is dropped = {}", Rc::strong_count(&a));
 }
--- 15 smart pointers/src/main.rs
+++ 15 smart pointers/src/main.rs
@@ -2,7 +2,8 @@ use smart_pointers::linked_list;
 use smart_pointers::my_box;
 
 fn main() {
-    linked_list::demo();
+    linked_list::recursion_demo();
+    linked_list::rc_demo();
     my_box::deref_demo();
     my_box::implicit_deref_demo();
 }
