a9be4154a6a77c2c74e03308f88b26a6da9e5684
Robert Buchberger <robert@buchberger.cc>
2022-05-19 16:56:41 +0200

    Webserver - multithreaded implementation (book)

--- 20 webserver/src/main.rs
+++ 20 webserver/src/main.rs
@@ -1,14 +1,20 @@
-use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,8 +25,15 @@ fn handle_connection(mut stream: TcpStream) {
     println!("{}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let css = b"GET /style.css HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "src/index.html")
+    } else if buffer.starts_with(css) {
+        ("HTTP/1.1 200 OK", "src/style.css")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "src/index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "src/404.html")
