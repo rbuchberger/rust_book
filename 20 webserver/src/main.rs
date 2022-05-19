f0b40e583ae4812b96d6a34d04e75d01def08980
Robert Buchberger <robert@buchberger.cc>
2022-05-19 15:43:46 +0200

    Webserver - basic, single threaded implementation

---
+++ 20 webserver/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "src/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "src/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{} \r\n Content-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
